989b7ca3ba4502aca84d9ee0353b4af172360fd6
Bob T <54815862+appjedi@users.noreply.github.com>
2024-12-01 19:18:57 -0800

    Update UserDAO.java

--- demo/src/main/java/net/appdojo/demo/dao/UserDAO.java
+++ demo/src/main/java/net/appdojo/demo/dao/UserDAO.java
@@ -7,11 +7,17 @@ import java.util.List;
 import net.appdojo.demo.models.User;
 
 public class UserDAO extends Database{
+	public static void main (String[]args)
+	{
+		UserDAO dao = new UserDAO();
+		User user = dao.auth("bob","Test1234");
+		System.out.println(user);
+	}
 	public User getUser(int id)
 	{
 		Database db = new Database();
 		try {
-			ResultSet rs = db.getResultSet("SELECT * FROM users");
+			ResultSet rs = db.getResultSet("SELECT * FROM users where id="+id);
         	
         	if (rs==null||!rs.next())
         	{
@@ -23,12 +29,41 @@ public class UserDAO extends Database{
         	user.setUserId(id);
         	user.setEmail(rs.getString("email"));
         	user.setUsername(rs.getString("username"));
+        	user.setFullName(rs.getString("full_name"));
         	user.setStatus(rs.getInt("status"));
         	user.setRoleId(rs.getInt("role_id"));
         	return user;
 		}catch (Exception ex)
 		{
 			return null;
+		}
+	}
+	public User auth(String un, String pw)
+	{
+		Database db = new Database();
+		try {
+			ResultSet rs = db.query("SELECT * FROM users where username=? AND password=?",un,pw);
+        	
+        	if (rs==null||!rs.next())
+        	{
+        		System.err.println ("Query failed");
+        		return null;
+        	}
+        	User user = new User();
+        	
+        	user.setUserId(rs.getInt("id"));
+        	user.setEmail(rs.getString("email"));
+        	user.setUsername(rs.getString("username"));
+        	user.setFullName(rs.getString("full_name"));
+        	user.setStatus(rs.getInt("status"));
+        	user.setRoleId(rs.getInt("role_id"));
+        	return user;
+		}catch (Exception ex)
+		{
+			ex.printStackTrace();
+			return null;
+		}finally {
+			db.close();
 		}
 	}
 	public List<User> getUsers()
@@ -46,9 +81,10 @@ public class UserDAO extends Database{
         	do {
 	        	User user = new User();
 	        	
-	        	user.setUserId(rs.getInt("user_id"));
+	        	user.setUserId(rs.getInt("id"));
 	        	user.setEmail(rs.getString("email"));
 	        	user.setUsername(rs.getString("username"));
+	        	user.setFullName(rs.getString("full_name"));
 	        	user.setStatus(rs.getInt("status"));
 	        	user.setRoleId(rs.getInt("role_id"));
 	        	users.add(user);
@@ -58,5 +94,18 @@ public class UserDAO extends Database{
 		{
 			return null;
 		}
+	}
+	public User create (User user)
+	{
+		Database db = new Database();
+		try {
+			String sp="call usp_user_save";
+			
+			return user;
+		}catch (Exception ex)
+		{
+			return null;
+		}
+		
 	}
 }
