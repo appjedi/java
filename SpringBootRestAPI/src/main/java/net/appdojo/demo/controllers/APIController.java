3a3fa7adb875ee3e1992ac4efb6d1006f594a2da
Bob T <54815862+appjedi@users.noreply.github.com>
2024-12-21 20:17:44 -0800

    update repo

--- SpringBootRestAPI/src/main/java/net/appdojo/demo/controllers/APIController.java
+++ SpringBootRestAPI/src/main/java/net/appdojo/demo/controllers/APIController.java
@@ -1,6 +1,5 @@
 package net.appdojo.demo.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -8,10 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import ch.qos.logback.core.joran.spi.HttpUtil.RequestMethod;
 import net.appdojo.demo.dao.UserDAO;
 import net.appdojo.demo.models.User;
 
