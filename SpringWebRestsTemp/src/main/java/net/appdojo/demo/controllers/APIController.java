d0376f86d397f11b9663204964a7b3254ee2f976
Bob T <54815862+appjedi@users.noreply.github.com>
2024-12-22 09:12:36 -0800

    update repo

---
+++ SpringWebRestsTemp/src/main/java/net/appdojo/demo/controllers/APIController.java
@@ -0,0 +1,107 @@
+package net.appdojo.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.appdojo.demo.models.User;
+import net.appdojo.demo.services.MainService;
+
+@RestController()
+@RequestMapping("/api")
+public class APIController {
+	@Autowired
+	MainService service;
+
+	@PostMapping("/auth")
+	@CrossOrigin()
+	public User auth(@RequestBody User user) {
+		try {
+			System.out.printf("1. post auth: \n%s\n", user);
+			User authUser = service.auth(user.getUsername(), user._pw());
+			System.out.printf("2. post auth: \n%s\n", authUser);
+			if (authUser == null) {
+				user.setStatus(0);
+				user.setUserId(0);
+				user.setRoleId(0);
+				user.setFullName("Auth Failed!");
+				return user;
+			}
+			return authUser;
+		} catch (Exception ex) {
+			System.err.println("post auth error:" + ex);
+			user.setStatus(-1);
+			user.setUserId(0);
+			user.setRoleId(0);
+			user.setFullName("error:" + ex.getMessage());
+			return user;
+		}
+	}
+
+	@PostMapping("/user")
+	@CrossOrigin()
+	public User postUser(@RequestBody User user) {
+		try {
+			service.save(user);
+			return user;
+		} catch (Exception ex) {
+			System.err.println("post auth error:" + ex);
+			user.setStatus(-1);
+			user.setUserId(0);
+			user.setRoleId(0);
+			user.setFullName("error:" + ex.getMessage());
+			return user;
+		}
+	}
+
+	@GetMapping("/user")
+	public User getUser() {
+		User user = new User(1, "testerb", "Test1234", "testerb@test.com", "Bob Tester", 2, 1);
+		return user;
+	}
+
+	@GetMapping("/user/{id}")
+	public User getUser(@PathVariable int id) {
+		User user = new User();
+
+		try {
+			user = service.getUser(id);
+			System.out.println(user);
+			return user;
+		} catch (Exception ex) {
+			return user;
+		}
+	}
+
+	@GetMapping("/users")
+	// @CrossOrigin(origins = "http://127.0.0.1:5500")
+	@CrossOrigin()
+	public List<User> getUsers() {
+
+		try {
+			List<User> users = service.getUsers();
+			return users;
+		} catch (Exception ex) {
+			return null;
+		}
+
+	}
+
+	// Another syntax to implement a
+	// GET method
+	@GetMapping("/info")
+	public String info() {
+		String str2 = "<html><body><font color=\"green\">" + "<h2>GeeksForGeeks is a Computer"
+				+ " Science portal for Geeks. " + "This portal has been " + "created to provide well written, "
+				+ "well thought and well explained " + "solutions for selected questions."
+				+ "</h2></font></body></html>";
+		return str2;
+	}
+}
---
+++ SpringWebRestsTemp/src/main/java/net/appdojo/demo/models/User.java
@@ -0,0 +1,92 @@
+package net.appdojo.demo.models;
+
+public class User {
+	int userId;
+	private String username;
+	private String password;
+	private String email;
+	private String fullName;
+	
+
+	private int roleId;
+	private int status;
+	
+	public User () {
+		this.userId=0;
+		
+	}
+	public User(int userId,String username, String password, String email, String fullName,int roleId, int status) {
+		super();
+		this.userId=userId;
+		this.username = username;
+		this.fullName = fullName;
+		this.password = password;
+		this.email = email;
+		this.roleId = roleId;
+		this.status = status;
+	}
+	private static String TO_STRING="{userId:%d, username:'%s', email:'%s',fullName:'%s',roleId:%d, status:%d}";
+	@Override
+	public String toString() {
+		return String.format(TO_STRING,this.userId,this.username, this.email,this.fullName,this.roleId, this.status);
+	}
+	private static String JSON="{'userId':%d, 'username':'%s', 'email':'%s','fullName':'%s','roleId':%d, 'status':%d}";
+	public String getJson ()
+	{
+		return String.format(JSON,this.userId,this.username, this.email,this.fullName,this.roleId, this.status);
+	}
+	public Object[]getValues()
+	{
+		Object[]values = {
+			this.userId,this.username,this.password,this.email,this.fullName,this.roleId, this.status
+		};
+		return values;
+	}
+	public int getUserId() {
+		return this.userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getPassword() {
+		return "*********";
+	}
+	public String _pw() {
+		return this.password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public int getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	
+}
