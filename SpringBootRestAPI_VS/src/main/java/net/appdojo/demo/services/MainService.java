851bf7575e3bfdbcc3d037d40f1aabd155e902c5
Bob T <54815862+appjedi@users.noreply.github.com>
2024-12-23 10:20:20 -0800

    update repo

---
+++ SpringBootRestAPI_VS/src/main/java/net/appdojo/demo/services/MainService.java
@@ -0,0 +1,32 @@
+package net.appdojo.demo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import net.appdojo.demo.dao.UserDAO;
+import net.appdojo.demo.models.User;
+
+@Service
+public class MainService {
+	@Autowired
+	UserDAO userDAO;
+	
+	public User getUser(int id)
+	{
+		return userDAO.getUser(id);
+	}
+	public User auth(String un, String pw)
+	{
+		return userDAO.auth(un,pw);
+	}
+	public List<User> getUsers()
+	{
+		return userDAO.getUsers();
+	}
+	public User save (User user)
+	{
+		return userDAO.save(user);
+	}
+}
