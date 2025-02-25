6b4b7aea970319474863a1afca14db5be70c306c
Bob T <54815862+appjedi@users.noreply.github.com>
2025-02-25 09:41:14 -0800

    update repo

--- training/Hello.java
+++ training/Hello.java
@@ -1,6 +1,9 @@
 public class Hello {
     public static void main(String[]args)
     {
-        System.out.println("Hello World!");
+        for (String name:args)
+        {
+            System.out.println("Hello "+name);
+        }
     }
 }
