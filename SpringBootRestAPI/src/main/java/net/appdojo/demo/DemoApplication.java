7542668eaf81f559e84792dda6ee8f1e1323785f
Bob T <54815862+appjedi@users.noreply.github.com>
2025-03-17 15:56:06 -0700

    update repo

--- SpringBootRestAPI/src/main/java/net/appdojo/demo/DemoApplication.java
+++ SpringBootRestAPI/src/main/java/net/appdojo/demo/DemoApplication.java
@@ -6,18 +6,21 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-
+/*
+ * Deploy Spring Boot Tomcat:
+ * https://www.youtube.com/watch?v=n_HCEw3Ffns 
+ * 
+ */
 
 @SpringBootApplication
-public class DemoApplication extends SpringBootServletInitializer{
+public class DemoApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	 @Override
+	 //@Override
         protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
             return builder.sources(DemoApplication.class);
         }
